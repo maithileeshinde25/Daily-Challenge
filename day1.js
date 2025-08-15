74a5a0b1c9b7466be79aed09e15604462f121fa6
maithileeshinde25 <maithileeshinde25@gmail.com>
2025-08-15 22:45:25 +0530

    day 1 sorted array

---
+++ day1.js
@@ -0,0 +1,22 @@
+function sortArray(arr) {
+    let low = 0, mid = 0, high = arr.length - 1;
+
+    while (mid <= high) {
+        if (arr[mid] === 0) { 
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+            low++;
+            mid++;
+        } 
+        else if (arr[mid] === 1) { 
+            mid++;
+        } 
+        else { 
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+            high--;
+        }
+    }
+    return arr;
+}
+
+
+console.log(sortArray([0,1,2,1,0,2,1,0]));
