92ada29795cdaa7e26a97b637b5b72090bf0e700
maithileeshinde25 <maithileeshinde25@gmail.com>
2025-08-30 17:12:34 +0530

    day 16

---
+++ day16.js
@@ -0,0 +1,15 @@
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+
+console.log(lcm(4, 6));         
